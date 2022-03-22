e63474fba9999ac6074793fd60be852ab5ee2cae
BHessel <hesselfitness@gmail.com>
2022-03-22 17:12:47 -0400

    backend done

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,2 +1,5 @@
 Rails.application.config.session_store :cookie_store, key:
 "_authentication_app", domain: "http://localhost:3001/"
+
+
+# Use this video when building for production: https://www.youtube.com/watch?v=1JRdaemJNh4&list=PLgYiyoyNPrv_yNp5Pzsx0A3gQ8-tfg66j&index=4
